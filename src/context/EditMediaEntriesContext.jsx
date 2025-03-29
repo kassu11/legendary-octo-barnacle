2ccdcc5daf671c17e1b6a643b90f9c27747a99f3
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-03-29 16:35:20 +0200

    Fix inputs

--- src/context/EditMediaEntriesContext.jsx
+++ src/context/EditMediaEntriesContext.jsx
@@ -6,32 +6,66 @@ import ScoreInput from "../components/media/ScoreInput";
 
 const EditMediaEntriesContext = createContext();
 
-function formState(initialData) {
+function formState(auth, initialData) {
+  assert(!initialData || auth, "Should not be able to edit if not authenticated");
   const [score, setScore] = createSignal();
+  const [status, setStatus] = createSignal();
+  const [format, setFormat] = createSignal();
+  const [progress, setProgress] = createSignal()
+  const [maxProgress, setMaxProgress] = createSignal()
+  const [startedAt, setStartedAt] = createSignal()
+  const [completedAt, setCompletedAt] = createSignal()
+  const [repeat, setRepeat] = createSignal()
+  const [notes, setNotes] = createSignal()
+  const [like, setLike] = createSignal()
+  const [hideFromStatus, setHideFromStatus] = createSignal()
+  const [mediaPrivate, setMediaPrivate] = createSignal()
 
-  function setState(data) {
+  function setState(auth, data) {
+    setFormat(auth?.data.data.Viewer.mediaListOptions.scoreFormat);
+
+    console.log(auth);
+
     setScore(data?.mediaListEntry?.score || 0);
+    setStatus(data?.mediaListEntry?.status || "none");
+    setProgress(data?.mediaListEntry?.progress || 0);
+    setMaxProgress(data?.episodes || data?.chapters || null);
+    setStartedAt(formatDateToInput(data?.mediaListEntry?.startedAt));
+    setCompletedAt(formatDateToInput(data?.mediaListEntry?.completedAt));
+    setRepeat(data?.mediaListEntry?.repeat || 0);
+    setNotes(data?.mediaListEntry?.notes || "");
   }
 
-  setState(initialData);
+  setState(auth, initialData);
 
-  return [{ score, setScore }, setState];
+  return [{ 
+    score, setScore, 
+    status, setStatus,
+    format, setFormat,
+    progress, setProgress,
+    maxProgress, setMaxProgress,
+    startedAt, setStartedAt,
+    completedAt, setCompletedAt,
+    repeat, setRepeat,
+    notes, setNotes,
+    like, setLike,
+    hideFromStatus, setHideFromStatus,
+    mediaPrivate, setMediaPrivate,
+  }, setState];
 }
 
 export function EditMediaEntriesProvider(props) {
   const [mediaListEntry, setMediaListEntry] = createSignal(undefined);
   const { accessToken, authUserData } = useAuthentication()
-
   const [state, setState] = formState();
 
   let editor;
 
   createEffect(() => {
+    setState(authUserData(), mediaListEntry());
     if (mediaListEntry()) {
-      assert(authUserData(), "Should not be able to edit if not authenticated");
       console.log("mediaListData", mediaListEntry());
       console.log("authUserData", authUserData());
-      setState(mediaListEntry());
       editor.showModal();
     }
   });
@@ -97,19 +131,19 @@ export function EditMediaEntriesProvider(props) {
             <h2>{mediaListEntry().title.userPreferred}</h2>
             <div>
               <label htmlFor="status">Status</label>
-              <select name="status" id="status">
-                <option value="none" selected={mediaListEntry().mediaListEntry?.status == null} disabled hidden>Select status</option>
-                <option value="COMPLETED" selected={mediaListEntry().mediaListEntry?.status === "COMPLETED"}>Completed</option>
-                <option value="CURRENT" selected={mediaListEntry().mediaListEntry?.status === "CURRENT"}>
+              <select name="status" id="status" value={state.status()} onChange={e => state.setStatus(e.target.value)}>
+                <option value="none" disabled hidden>Select status</option>
+                <option value="COMPLETED">Completed</option>
+                <option value="CURRENT">
                   <Switch>
                     <Match when={mediaListEntry().type === "MANGA"}> Reading</Match>
                     <Match when={mediaListEntry().type === "ANIME"}> Watching</Match>
                   </Switch>
                 </option>
-                <option value="DROPPED" selected={mediaListEntry().mediaListEntry?.status === "DROPPED"}>Dropped</option>
-                <option value="PAUSED" selected={mediaListEntry().mediaListEntry?.status === "PAUSED"}>Paused</option>
-                <option value="PLANNING" selected={mediaListEntry().mediaListEntry?.status === "PLANNING"}>Planning</option>
-                <option value="REPEATING" selected={mediaListEntry().mediaListEntry?.status === "REPEATING"}>
+                <option value="DROPPED">Dropped</option>
+                <option value="PAUSED">Paused</option>
+                <option value="PLANNING">Planning</option>
+                <option value="REPEATING">
                   <Switch>
                     <Match when={mediaListEntry().type === "MANGA"}>Rereading</Match>
                     <Match when={mediaListEntry().type === "ANIME"}>Rewatching</Match>
@@ -119,27 +153,57 @@ export function EditMediaEntriesProvider(props) {
             </div>
             <div>
               <label htmlFor="score">Score</label>
-              <ScoreInput value={state.score()} onChange={state.setScore} format={authUserData().data.data.Viewer.mediaListOptions.scoreFormat} />
+              {console.log(state.format())}
+              <ScoreInput value={state.score()} onChange={state.setScore} format={state.format()} />
             </div>
             <div>
               <label htmlFor="progress">Episode Progress</label>
-              <input type="number" id="progress" name="progress" value={mediaListEntry().mediaListEntry?.progress || 0} />
+              <input 
+                type="number" 
+                inputMode="numeric" 
+                id="progress" 
+                name="progress" 
+                min="0" 
+                max={state.maxProgress()}
+                value={state.progress()} 
+                onChange={e => state.setProgress(e.target.value)} 
+                onBlur={e => e.target.value = state.progress()} 
+                onBeforeInput={e => {
+                  if (e.data?.toLowerCase().includes("e")) {
+                    e.preventDefault();
+                  }
+                }}
+              />
             </div>
             <div>
               <label htmlFor="start-date">Start date</label>
-              <input type="date" id="start-date" name="startedAt" value={formatDateToInput(mediaListEntry().mediaListEntry?.startedAt)} />
+              <input type="date" id="start-date" name="startedAt" value={state.startedAt()} onChange={e => state.setStartedAt(e.target.value)}/>
             </div>
             <div>
               <label htmlFor="end-date">Finish date</label>
-              <input type="date" id="end-date" name="completedAt" value={formatDateToInput(mediaListEntry().mediaListEntry?.completedAt)} />
+              <input type="date" id="end-date" name="completedAt" value={state.completedAt()} onChange={e => state.setCompletedAt(e.target.value)}/>
             </div>
             <div>
               <label htmlFor="repeat">Total Rewatches</label>
-              <input type="number" id="repeat" name="repeat" value={mediaListEntry().mediaListEntry?.repeat || 0} />
+              <input 
+                type="number" 
+                inputMode="numeric" 
+                id="repeat" 
+                name="repeat" 
+                min="0" 
+                value={state.repeat()} 
+                onChange={e => state.setRepeat(e.target.value)} 
+                onBlur={e => e.target.value = state.repeat()} 
+                onBeforeInput={e => {
+                  if (e.data?.toLowerCase().includes("e")) {
+                    e.preventDefault();
+                  }
+                }}
+              />
             </div>
             <div>
               <label htmlFor="notes">Notes</label>
-              <textarea type="text" id="notes" name="notes" value={mediaListEntry().mediaListEntry?.notes || ""} />
+              <textarea type="text" id="notes" name="notes" value={state.notes()} onChange={e => state.setNotes(e.target.value)} />
             </div>
             <h3>Advanced Scores</h3>
             <button type="button">Delete</button>
