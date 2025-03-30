0903260eb5731998e3b59be33501bd84b652e186
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-03-30 15:22:25 +0300

    Update mangaInfo page

--- src/pages/MangaInfo.jsx
+++ src/pages/MangaInfo.jsx
@@ -1,87 +1,133 @@
 import { useParams } from "@solidjs/router";
 import { A } from "../components/CustomA";
 import api from "../utils/api";
-import { Switch, Match, Show } from "solid-js";
-import { assert } from "../utils/assert";
+import { Show, createEffect, createSignal } from "solid-js";
+import style from "./AnimeInfo.module.scss";
+import { Markdown } from "../components/Markdown";
+import { useAuthentication } from "../context/AuthenticationContext";
+import Banner from "../components/media/Banner";
+import Rankings from "../components/media/Rankings";
+import Genres from "../components/media/Genres";
+import Tags from "../components/media/Tags";
+import Header from "../components/media/Header";
+import Characters from "../components/media/Characters";
+import Friends from "../components/media/Friends";
+import { assert } from "../utils/assert.js";
+import { useEditMediaEntries } from "../context/EditMediaEntriesContext.jsx";
 
 
 function Manga() {
   const params = useParams();
-  const id = Number(params.id);
+  const { accessToken } = useAuthentication();
+  const [id, setId]= createSignal(Number(params.id));
 
-  assert(!Number.isNaN(id), "ID should not be NaN");
-  const [mangaData] = api.anilist.mediaId(id);
+  assert(!Number.isNaN(id()), "ID should not be NaN");
+  const [animeData] = api.anilist.mediaId(id, accessToken);
+  const [friendScoreData] = api.anilist.friendsMediaScore(accessToken, id, {page: 1, perPage: 8});
+
+  createEffect(() => {
+    setId(Number(params.id))
+  });
+
+  return (
+    <Show when={animeData()}>
+      <MangaInfo anime={animeData().data.data.Media} friend={friendScoreData()?.data.data.Page}></MangaInfo>
+    </Show>
+  )
+}
+
+function MangaInfo(props) {
+  assert(props.anime, "Data missing");
+  assert(props.anime?.id, "Id missing");
+
+  const [malId, setMalId]= createSignal(undefined);
+  const [malData] = api.myAnimeList.mangaById(malId);
+  const { accessToken } = useAuthentication();
+  const { setMediaListEntry } = useEditMediaEntries();
+  
+  createEffect(() => {
+    if (props.anime.idMal) {
+      setMalId(props.anime.idMal);
+    }
+  });
+
+  createEffect(() => {
+    console.log(malData());
+  })
+
+  console.log(props.anime);
 
   return (
     <>
-      <h1>Anime {params.id}</h1>
-      <div>
-        <Show when={mangaData.loading}>
-          <p>Loading...</p>
-        </Show>
-        <Switch>
-          <Match when={mangaData.error}>
-            <span>Error: {mangaData.error}</span>
-          </Match>
-          <Match when={mangaData()}>
-            <MangaInfo data={mangaData().data.data.Media}></MangaInfo>
-          </Match>
-        </Switch>
+      <Banner src={props.anime.bannerImage} />
+      <div class={style.container}>
+        <div class={style.left}>
+          <img src={props.anime.coverImage.large} alt="Cover" class={style.cover} />
+          <Show when={accessToken()}>
+            <button onClick={() => {
+              const [data] = api.anilist.mediaListEntry(accessToken, props.anime.id);
+              createEffect(() => {
+                if (data()) {
+                  setMediaListEntry(data().data.data.Media);
+                }
+              })
+            }}>{props.anime.mediaListEntry?.status || "Edit"}</button>
+          </Show>
+          <Show when={props.anime.idMal}>
+            {console.log("ID", props.anime.idMal, malData())}
+            <Show when={malData()} fallback={<p>MAL score: loading</p>}>
+              <p>MAL score: {malData().data.data.score}</p>
+            </Show>
+          </Show>
+          <Rankings rankings={props.anime.rankings} />
+          <Genres genres={props.anime.genres} />
+          <Tags tags={props.anime.tags} />
+        </div>
+        <div class={style.main}>
+          <Header 
+            averageScore={props.anime.averageScore} 
+            favourites={props.anime.favourites}
+            format={props.anime.format}
+            meanScore={props.anime.meanScore} 
+            popularity={props.anime.popularity}
+            season={props.anime.season}
+            seasonYear={props.anime.seasonYear}
+            source={props.anime.source}
+            ratingUsers={props.anime.stats.scoreDistribution.reduce((acc, v) => v.amount + acc, 0)}
+            stats={props.anime.stats}
+            status={props.anime.status}
+          />
+          <div>
+            <h1>{props.anime.title.userPreferred}</h1>
+            <Markdown children={props.anime.description}/>
+          </div>
+          <div class={style.relationContainer}>
+            <h2>Relations</h2>
+            <ol>
+              <For each={props.anime.relations.edges}>{relation => (
+                <li>
+                  <A 
+                    href={"/" + relation.node.type.toLowerCase() + "/" + relation.node.id + "/" + relation.node.title.userPreferred}
+                    class={style.relation}
+                  >
+                    <img src={relation.node.coverImage.large} alt="Cover" />
+                    <div class={style.content}>
+                      <p class={style.type}>{relation.relationType}</p>
+                      <p class={style.lineClamp}>{relation.node.title.userPreferred}</p>
+                      <p class={style.format}>{relation.node.format} - {relation.node.status}</p>
+                    </div>
+                  </A>
+                </li>
+              )}</For>
+            </ol>
+          </div>
+          <Characters characters={props.anime.characterPreview.edges} />
+          <Friends friend={props.friend} media={props.anime} type="anime" />
+        </div>
       </div>
     </>
   )
 }
 
-function MangaInfo(props) {
-  assert(props.data, "Data missing");
-  assert(props.data?.id, "Id missing");
-
-  const anime = props.data;
-  return (
-    <ul>
-      <li>
-        <b>Banner:</b>
-        <img src={anime.bannerImage} alt="Banner" />
-      </li>
-      <li><b>Title EN:</b> {anime.title.english}</li>
-      <li><b>Title native:</b> {anime.title.native}</li>
-      <li><b>Title romaji:</b> {anime.title.romaji}</li>
-      <li><a target="_blank" href={`https://myanimelist.net/manga/${anime.idMal}/`}>MyAnimeList</a></li>
-      <li><a target="_blank" href={`https://anilist.co/manga/${anime.id}/`}>AniList</a></li>
-      <li>
-        <b>Cover:</b>
-        <img src={anime.coverImage.large} alt="Cover" />
-      </li>
-      <li><b>Description:</b> {anime.description}</li>
-      <li><b>Type:</b> {anime.type}</li>
-      <li><b>Season:</b> {anime.season}</li>
-      <li><b>SeasonYear:</b> {anime.seasonYear}</li>
-      <li><b>status:</b> {anime.status}</li>
-      <li><b>format:</b> {anime.format}</li>
-      <li><b>date:</b> {JSON.stringify(anime.startDate)}</li>
-      <li><b>Mean Score:</b> {anime.meanScore / 10}</li>
-      <li><b>Average Score:</b> {anime.averageScore / 10}</li>
-      <li>
-        <b><A href="characters">Characters:</A></b>
-        <For each={anime.characterPreview.edges}>{char => (
-          <div>
-            <A href={"/ani/character/" + char.node.id}>
-              <span>{char.node.name.userPreferred}</span><br />
-              <img src={char.node.image.large} alt="character" /><br />
-            </A>
-            <For each={char.voiceActors}>{actor => (
-              <A href={"/ani/staff/" + actor.id}>
-                <span>{actor.name.userPreferred}</span><br />
-                <img src={actor.image.large} alt="character" /><br />
-              </A>
-            )}</For>
-          </div>
-        )}</For>
-      </li>
-    </ul>
-  )
-
-}
-
-
 export default Manga
+
