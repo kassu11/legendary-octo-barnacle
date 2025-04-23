c3436d519a5aa38d56dbbb099506a394375290a3
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-04-23 20:35:48 +0300

    User profile handle error and remove unused stuff

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,22 +2,9 @@ import { A, useParams } from "@solidjs/router";
 import api from "../utils/api.js";
 import { For, Show, createEffect, createSignal, on } from "solid-js";
 import "./User.scss";
-import { Markdown } from "../components/Markdown.jsx";
 import { useAuthentication } from "../context/AuthenticationContext.jsx";
-import Banner from "../components/media/Banner.jsx";
-import ExternalLinks from "../components/media/ExternalLinks.jsx";
-import ExtraInfo from "../components/media/ExtraInfo.jsx";
-import Rankings from "../components/media/Rankings.jsx";
-import Genres from "../components/media/Genres.jsx";
-import Tags from "../components/media/Tags.jsx";
-import Header from "../components/media/Header.jsx";
-import Characters from "../components/media/Characters.jsx";
-import Friends from "../components/media/Friends.jsx";
-import AnimeThemes from "../components/media/AnimeThemes.jsx";
 import { assert } from "../utils/assert.js";
-import { useEditMediaEntries } from "../context/EditMediaEntriesContext.jsx";
 import { formatTimeToDate, formatTitleToUrl, numberCommas } from "../utils/formating.js";
-import { FavouriteToggle } from "../components/FavouriteToggle.jsx";
 import { ActivityCard } from "../components/Activity.jsx";
 
 export default function User() {
@@ -26,15 +13,20 @@ export default function User() {
   const [userData] = api.anilist.userByName(() => params.name, accessToken);
 
   return (
-    <Show when={userData()}>
-      <Content user={userData().data.data.User} />
-    </Show>
+    <Switch>
+      <Match when={userData()?.data}>
+        <Content user={userData().data.data.User} />
+      </Match>
+      <Match when={userData()?.error}>
+        <div>Error user not found</div>
+      </Match>
+    </Switch>
   )
 }
 
 function Content(props) {
   const { accessToken } = useAuthentication();
-  const [activity, { mutate: mutateActivity }] = api.anilist.activityByUserId(() => props.user.id || undefined, accessToken);
+  const [activity, { mutateCache: mutateActivityCache }] = api.anilist.activityByUserId(() => props.user.id || undefined, accessToken);
 
   return (
     <div class="user-page" style={{"--user-color": props.user.options.profileColor}}>
@@ -177,7 +169,7 @@ function Content(props) {
           </div>
           <div class="user-profile-activity">
             <For each={activity()?.data.data.Page.activities}>{activity => (
-              <ActivityCard activity={activity} mutateCache={mutateActivity} />
+              <ActivityCard activity={activity} mutateCache={mutateActivityCache} />
             )}</For>
           </div>
         </div>
