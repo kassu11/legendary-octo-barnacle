880aba0861474e4e05702785c9725486a9dee5d4
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-04-24 20:44:53 +0300

    Refactor media page genres css

--- src/components/media/Genres.jsx
+++ src/components/media/Genres.jsx
@@ -1,16 +1,28 @@
 import { For } from "solid-js";
-import style from "./Genres.module.scss";
+import "./Genres.scss";
+import { A } from "@solidjs/router";
+import { Show } from "solid-js";
 
 const Genres = (props) => {
   return (
-    <div class={style.genreContainer}>
-      <h2>Genres</h2>
-      <ul>
-        <For each={props.genres}>{genre => (
-          <li class={style.genre}>{genre}</li>
-        )}</For>
-      </ul>
-    </div>
+    <Show when={props.genres?.length}>
+      <div class="media-page-genre-container">
+        <h2>
+          <A href={"/search/" + props.type.toLowerCase() + "?" + props.genres.map(genre => "genres=" + genre).join("&")}>
+            Genres
+          </A>
+        </h2>
+        <ul>
+          <For each={props.genres}>{genre => (
+            <li class="media-page-genre">
+              <A href={"/search/" + props.type.toLowerCase() + "?genres=" + genre}>
+                {genre}
+              </A>
+            </li>
+          )}</For>
+        </ul>
+      </div>
+    </Show>
   );
 };
 
