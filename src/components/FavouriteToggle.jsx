acc7b89171695c5dd1c0710d978b0a2520cebbd3
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-04-25 01:40:28 +0300

    Add scuffed mobile support for media pages

--- src/components/FavouriteToggle.jsx
+++ src/components/FavouriteToggle.jsx
@@ -3,6 +3,7 @@ import api from "../utils/api";
 import style from "./FavouriteToggle.module.scss";
 import { assert } from "../utils/assert";
 import { useAuthentication } from "../context/AuthenticationContext";
+import { compactNumber } from "../utils/formating.js";
 
 export function FavouriteToggle(props) {
   const { accessToken } = useAuthentication();
@@ -32,6 +33,9 @@ export function FavouriteToggle(props) {
         }
       }}/>
       <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
+      <Show when={props.favourites != undefined}>
+        {compactNumber(props.favourites)}
+      </Show>
       <span class="visually-hidden">Favourite</span>
     </label>
   );
--- src/pages/MediaInfo.jsx
+++ src/pages/MediaInfo.jsx
@@ -16,7 +16,7 @@ import Friends from "../components/media/Friends.jsx";
 import AnimeThemes from "../components/media/AnimeThemes.jsx";
 import { assert } from "../utils/assert.js";
 import { useEditMediaEntries } from "../context/EditMediaEntriesContext.jsx";
-import { formatTitleToUrl } from "../utils/formating.js";
+import { capitalize, compactNumber, formatMediaFormat, formatMediaSource, formatMediaStatus, formatTitleToUrl, numberCommas } from "../utils/formating.js";
 import { FavouriteToggle } from "../components/FavouriteToggle.jsx";
 
 export function AnimeInfo() {
@@ -66,7 +66,7 @@ function MediaInfo(props) {
   assert(props.media, "Data missing");
   assert(props.media?.id, "Id missing");
 
-  const { accessToken } = useAuthentication();
+  const { accessToken, authUserData } = useAuthentication();
   const { openEditor } = useEditMediaEntries();
   const [isFavourite, setIsFavourite] = createSignal(props.media?.isFavourite ?? false);
   
@@ -77,9 +77,58 @@ function MediaInfo(props) {
   return (
     <>
       <Banner src={props.media.bannerImage} />
+      {console.log("MAL:", props.malData)}
       <div class="media-page-content">
         <aside class="media-page-left-aside">
           <img src={props.media.coverImage.large} alt="Cover" class="media-page-cover" />
+          <div class="media-page-score-container">
+            <div>
+              <p>Mean</p>
+              <span>
+                <Show when={props.media.meanScore > 0} fallback="N/A">
+                  {((props.media.meanScore || 0) / 10).toFixed(1)}
+                </Show>
+              </span>
+            </div>
+            <div>
+              <p>Average</p>
+              <span>
+                <Show when={props.media.averageScore > 0} fallback="N/A">
+                  {((props.media.averageScore || 0) / 10).toFixed(1)}
+                </Show>
+              </span>
+            </div>
+            <p class="media-page-score-anilist-users">
+              <Show when={props.media.stats.scoreDistribution?.reduce((acc, v) => v.amount + acc, 0)} fallback="-">
+                {numberCommas(props.media.stats.scoreDistribution?.reduce((acc, v) => v.amount + acc, 0))}
+              </Show>
+              {" "}Users
+            </p>
+            <Switch>
+              <Match when={props.malData}>
+                <div>
+                  <p>MAL</p>
+                  <span>
+                    <Show when={props.malData.data?.data.score > 0} fallback="N/A">
+                      {((props.malData.data.data.score || 0)).toFixed(2)}
+                    </Show>
+                  </span>
+                </div>
+              </Match>
+              <Match when={props.media.idMal}>
+                <div>
+                  <p>MAL</p>
+                  <span>...</span>
+                </div>
+              </Match>
+            </Switch>
+            <p>
+              <Show when={props.malData?.data?.data.scored_by} fallback="-">
+                {numberCommas(props.malData.data?.data.scored_by)}
+              </Show>
+              {" "}Users
+            </p>
+          </div>
           <Show when={accessToken()}>
             <button onClick={() => {
               openEditor(props.media, {
@@ -97,16 +146,12 @@ function MediaInfo(props) {
               onChange={setIsFavourite} 
               animeId={props.media.type === "ANIME" ? props.media.id : undefined}
               mangaId={props.media.type === "MANGA" ? props.media.id : undefined}
+              favourites={props.media.favourites}
               mutateCache={(isFavourite) => props.setMediaData(v => {
                 v.data.data.Media.isFavourite = isFavourite;
                 return v;
               })} 
             />
-          </Show>
-          <Show when={props.media.idMal}>
-            <Show when={props.malData} fallback={<p>MAL score: loading</p>}>
-              <p>MAL score: {props.malData.data.data.score}</p>
-            </Show>
           </Show>
           <ExternalLinks media={props.media}/>
           <ExtraInfo media={props.media}/>
@@ -115,12 +160,33 @@ function MediaInfo(props) {
           <Tags tags={props.media.tags} />
         </aside>
         <section class="media-page-main">
-          <Header 
-            {...props.media} 
-            ratingUsers={props.media.stats.scoreDistribution?.reduce((acc, v) => v.amount + acc, 0)}
-          />
-          <div>
+          <div class="media-page-title-info">
             <h1>{props.media.title.userPreferred}</h1>
+            <ul class="media-page-info-list">
+              <Switch>
+                <Match when={props.media.type === "MANGA"}>
+                  <li>{(props.media.startDate?.year) || "TBA"}</li>
+                </Match>
+                <Match when={props.media.type === "ANIME"}>
+                  <li>{capitalize(props.media.season) || "TBA"} {props.media.seasonYear}</li>
+                </Match>
+              </Switch>
+              <li>
+                {formatMediaFormat(props.media.format)}
+                <Switch>
+                  <Match when={props.media.countryOfOrigin === "CN"}> (Chinese)</Match>
+                  <Match when={props.media.countryOfOrigin === "TW"}> (Taiwanese)</Match>
+                </Switch>
+              </li>
+              <li>{formatMediaStatus(props.media.status)}</li>
+            </ul>
+            <ul>
+              <li>Source: {formatMediaSource(props.media.source)}</li>
+              <li>Members: {numberCommas(props.media.popularity)}</li>
+              <li>Favourites: {numberCommas(props.media.favourites)}</li>
+            </ul>
+          </div>
+          <div class="media-page-description">
             <Markdown children={props.media.description}/>
           </div>
           <div class="media-page-relation-container">
