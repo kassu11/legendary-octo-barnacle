1829ea31717538ff4ba049d3ef10236f78c2df91
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-04-29 09:43:17 +0300

    Add link to friend scores component

--- src/components/media/Friends.jsx
+++ src/components/media/Friends.jsx
@@ -2,6 +2,7 @@ import { For, Show } from "solid-js";
 import Status from "./Status";
 import Score from "./Score";
 import style from "./Friends.module.scss";
+import { A } from "@solidjs/router";
 
 function Friends(props) {
   return (
@@ -9,11 +10,13 @@ function Friends(props) {
       <div class={style.friendContainer}>
         <ul>
           <For each={props.friend.mediaList}>{friend => (
-            <li class={style.friend}>
-              <img src={friend.user.avatar.large} alt="User profile" />
-              <p>{friend.user.name}</p>
-              <Status friend={friend} media={props.media} type={props.type} />
-              <Score format={friend.user.mediaListOptions.scoreFormat} score={friend.score} />
+            <li>
+              <A class={style.friend} href={"/user/" + friend.user.name}>
+                <img src={friend.user.avatar.large} alt="User profile" />
+                <p>{friend.user.name}</p>
+                <Status friend={friend} media={props.media} type={props.type} />
+                <Score format={friend.user.mediaListOptions.scoreFormat} score={friend.score} />
+              </A>
             </li>
           )}</For>
         </ul>
