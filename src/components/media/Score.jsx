49fc139d01ef25372751444f7478428d12394324
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-04-30 20:54:43 +0300

    Add repeat to friend scores

--- src/components/media/Score.jsx
+++ src/components/media/Score.jsx
@@ -1,22 +1,22 @@
 import Star from "../../assets/Star";
 import { Show, Switch, Match } from "solid-js";
-import style from "./Score.module.scss";
+import "./Score.scss";
 import Emoji from "../../assets/Emoji";
 
 function Score(props) {
   return (
     <Show when={props.score !== 0}>
-      <div class={style.scoreContainer}>
+      <div class="score-component-container">
         <Switch>
           <Match when={props.format === "POINT_10"}>{props.score}/10</Match>
           <Match when={props.format === "POINT_100"}>{props.score}/100</Match>
           <Match when={props.format === "POINT_10_DECIMAL"}>{props.score}/10</Match>
-          <Match when={props.format === "POINT_5"}>{props.score}/5 <Star class={style.scoreStar} /></Match>
+          <Match when={props.format === "POINT_5"}>{props.score}/5 <Star class="score-star" /></Match>
           <Match when={props.format === "POINT_3"}>
             <Switch>
-              <Match when={props.score === 1}><Emoji class={style.scoreEmoji} score={80} /></Match>
-              <Match when={props.score === 2}><Emoji class={style.scoreEmoji} score={70} /></Match>
-              <Match when={props.score === 3}><Emoji class={style.scoreEmoji} score={0} /></Match>
+              <Match when={props.score === 1}><Emoji class="score-emoji" score={80} /></Match>
+              <Match when={props.score === 2}><Emoji class="score-emoji" score={70} /></Match>
+              <Match when={props.score === 3}><Emoji class="score-emoji" score={0} /></Match>
             </Switch>
           </Match>
         </Switch>
