e598909fc1a4f4cd08f505828ea8f7056e434caa
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-05-04 14:36:42 +0300

    Fix inconsistend async behavior on debounce

--- src/utils/scheduled.js
+++ src/utils/scheduled.js
@@ -1,35 +1,39 @@
 export const leadingAndTrailingDebounce = (callback, ms, bufferSize = 1) => {
   const buffer = [];
   let timeout = null;
+  let runningCallback = false;
   async function trigger(...callbackArgs) {
-    if (timeout === null) {
+    buffer.push(async () => {
+      buffer.shift();
+      runningCallback = true;
       await callback(...callbackArgs);
-      timeout = setTimeout(addFromBuffer, ms);
-    } else {
-      buffer.push(async () => {
-        await callback(...callbackArgs);
-        buffer.shift();
-        addFromBuffer();
-      });
+      runningCallback = false;
+      addFromBuffer(ms);
+    });
 
-      if (buffer.length == bufferSize + 1) {
-        buffer.shift();
-      }  
-
-      addFromBuffer();
+    if (buffer.length == bufferSize + 1) {
+      buffer.shift();
     }
+    if (runningCallback) { return; }
+    if (timeout === null) { addFromBuffer(); } 
+    else { addFromBuffer(ms); }
   }
 
   trigger.bufferSize = () => buffer.length;
 
   return trigger;
 
-  function addFromBuffer() {
+  function addFromBuffer(ms) {
     const bufCallback = buffer[0];
     clearTimeout(timeout);
     timeout = null;
-    if (bufCallback) {
+    if (!bufCallback) { return; }
+
+    if(ms) {
       timeout = setTimeout(bufCallback, ms);
+    } else {
+      runningCallback = true;
+      bufCallback();
     }
   }
 }
