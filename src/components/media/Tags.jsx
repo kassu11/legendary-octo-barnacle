7bc1865125e76d3ba2970b3376e34e8c2fff9490
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-05-17 12:55:50 +0300

    Add show spoiler tags button #51

--- src/components/media/Tags.jsx
+++ src/components/media/Tags.jsx
@@ -1,18 +1,38 @@
-import { For } from "solid-js";
+import { createEffect, createSignal, For, on, Show } from "solid-js";
 import style from "./Tags.module.scss";
 
 const Tags = (props) => {
+  const [showSpoilers, setShowSpoilers] = createSignal(false);
+
+  createEffect(on(() => props.tags, () => {
+    setShowSpoilers(false);
+  }));
+
   return (
-    <div class={style.tagContainer}>
-      <h2>Tags</h2>
-      <ol>
-        <For each={props.tags}>{tag => (
-          <li 
-            classList={{[style.tag]: true, [style.spoiler]: tag.isMediaSpoiler || tag.isGeneralSpoiler}} 
-            title={tag.description}>{tag.name} <span>{tag.rank}%</span></li>
-        )}</For>
-      </ol>
-    </div>
+    <Show when={props.tags.length}>
+      <div class={style.tagContainer}>
+        <h2>Tags</h2>
+        <ol>
+          <For each={props.tags}>{tag => (
+            <li 
+              classList={{
+                [style.tag]: true, 
+                [style.spoiler]: tag.isMediaSpoiler || tag.isGeneralSpoiler,
+                hidden: (tag.isMediaSpoiler || tag.isGeneralSpoiler) && !showSpoilers()
+              }} 
+              title={tag.description}
+            >
+              {tag.name} <span>{tag.rank}%</span>
+            </li>
+          )}</For>
+          <Show when={props.tags.some(tag => tag.isMediaSpoiler || tag.isGeneralSpoiler)}>
+            <button onClick={() => setShowSpoilers(state => !state)}>
+              <Show when={!showSpoilers()} fallback="Hide spoilers">Show spoilers</Show>
+            </button>
+          </Show>
+        </ol>
+      </div>
+    </Show>
   );
 };
 
