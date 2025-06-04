4fec05577165d05c5e62641c97744872b7be296c
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-06-04 21:21:39 +0300

    Fix user profile favourite staff component

--- src/context/providers.js
+++ src/context/providers.js
@@ -2,3 +2,6 @@ import { createContext, useContext } from "solid-js";
 
 export const SearchBarContext = createContext();
 export const useSearchBar = () => useContext(SearchBarContext);
+
+export const UserContext = createContext();
+export const useUser = () => useContext(UserContext);
--- src/pages/User/Stats/Genres.jsx
+++ src/pages/User/Stats/Genres.jsx
@@ -1,11 +1,11 @@
 import { A, useParams } from "@solidjs/router";
-import { useUser } from "../../User";
 import { useAuthentication } from "../../../context/AuthenticationContext";
 import api from "../../../utils/api";
 import { formatTitleToUrl, numberCommas, plural } from "../../../utils/formating";
 import { createEffect, createSignal, on } from "solid-js";
 import "./Genres.scss";
 import { createStore, reconcile } from "solid-js/store";
+import { useUser } from "../../../context/providers";
 
 export function StatsAnimeGenres() {
   const params = useParams();
