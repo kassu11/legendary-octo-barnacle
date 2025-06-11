0d7d8c6c9a9bf114ee32a54a8a64445df2bf5a37
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-06-11 19:53:14 +0300

    Fix current and repeat flavor text for manga

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -804,7 +804,7 @@ function AniCard(props) {
                   <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M12 192h424c6.6 0 12 5.4 12 12v260c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V204c0-6.6 5.4-12 12-12zm436-44v-36c0-26.5-21.5-48-48-48h-48V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H160V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v36c0 6.6 5.4 12 12 12h424c6.6 0 12-5.4 12-12z"></path></svg>
                 </button>
               </li>
-              <li class="item" label="Set to watching">
+              <li class="item" label={"Set to " + (props.card.type === "ANIME" ? "watching" : "reading")}>
                 <button onClick={e => {
                   e.preventDefault();
                   api.anilist.mutateMedia(accessToken(), { mediaId: props.card.id, status: "CURRENT" });
@@ -820,7 +820,7 @@ function AniCard(props) {
                   <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
                 </button>
               </li>
-              <li class="item" label="Set to rewatching">
+              <li class="item" label={"Set to " + (props.card.type === "ANIME" ? "rewatching" : "rereading")}>
                 <button onClick={e => {
                   e.preventDefault();
                   api.anilist.mutateMedia(accessToken(), { mediaId: props.card.id, status: "REPEAT" });
