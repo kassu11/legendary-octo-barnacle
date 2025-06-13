3a5971dde30520448ec1e689456a43afde8b3fe3
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-06-13 22:07:17 +0300

    feat: ✨ Media page tag links

--- src/components/media/Tags.jsx
+++ src/components/media/Tags.jsx
@@ -1,5 +1,6 @@
 import { createEffect, createSignal, For, on, Show } from "solid-js";
 import style from "./Tags.module.scss";
+import { A } from "@solidjs/router";
 
 const Tags = (props) => {
   const [showSpoilers, setShowSpoilers] = createSignal(false);
@@ -8,10 +9,32 @@ const Tags = (props) => {
     setShowSpoilers(false);
   }));
 
+  function nonSpoilerAndHighRankTags() {
+    const tags = [];
+    for(const tag of props.tags) {
+      if (tag.rank < 90 && tags.length >= 3) {
+        break;
+      }
+      if (showSpoilers() || (!tag.isMediaSpoiler && !tag.isGeneralSpoiler)) {
+        tags.push(`genre=${tag.name}`);
+      }
+    }
+    return tags;
+  }
+
   return (
     <Show when={props.tags.length}>
       <div class={style.tagContainer}>
-        <h2>Tags</h2>
+        <div class="flex-space-between">
+          <A href={"/search/" + props.type.toLowerCase() + "?" + nonSpoilerAndHighRankTags().join("&")}>
+            <h2>Tags</h2>
+          </A>
+          <Show when={props.tags.some(tag => tag.isMediaSpoiler || tag.isGeneralSpoiler)}>
+            <button onClick={() => setShowSpoilers(state => !state)}>
+              <Show when={!showSpoilers()} fallback="Hide spoilers">Show spoilers</Show>
+            </button>
+          </Show>
+        </div>
         <ol>
           <For each={props.tags}>{tag => (
             <li 
@@ -22,14 +45,9 @@ const Tags = (props) => {
               }} 
               title={tag.description}
             >
-              {tag.name} <span>{tag.rank}%</span>
+              <A href={ "/search/" + props.type.toLowerCase() + "?genre=" + tag.name + "&rank=" + tag.rank}>{tag.name} <span>{tag.rank}%</span></A>
             </li>
           )}</For>
-          <Show when={props.tags.some(tag => tag.isMediaSpoiler || tag.isGeneralSpoiler)}>
-            <button onClick={() => setShowSpoilers(state => !state)}>
-              <Show when={!showSpoilers()} fallback="Hide spoilers">Show spoilers</Show>
-            </button>
-          </Show>
         </ol>
       </div>
     </Show>
