efcf447b2177d1b600af61bf41a121f8818bc1f7
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-06-20 15:42:47 +0300

    fix: 🐛 Remove debug mode in production

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import * as queries from "./querys";
 import { assert } from "./assert";
 import { getDates } from "./dates";
 import { TokenBucket } from "./TokenBucket";
-const DEBUG = !location.origin.includes("localhost");
+const DEBUG = location.origin.includes("localhost");
 
 const reloadCache = cacheBuilder({ storeName: "results", type:"reload", expiresInSeconds: 60 * 60 * 24 * 365 });
 const fastReload = cacheBuilder({ storeName: "results", type:"reload", expiresInSeconds: 60 * 60 * 7 });
