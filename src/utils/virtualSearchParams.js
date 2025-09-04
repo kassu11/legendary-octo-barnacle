55b366f40bed69c21c4fe11dbec5bcc4399d4103
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-09-04 19:16:46 +0300

    refactor: 🧩 Move all virtualSearchParams function to global scope

--- src/utils/virtualSearchParams.js
+++ src/utils/virtualSearchParams.js
@@ -40,69 +40,72 @@ export function useVirtualSearchParams() {
   const location = useLocation();
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  return [virtualSearchParams, setVirtualSearchParams];
+  return [
+    (key) => virtualSearchParams(searchParams, params, key),
+    (sParams, options) => setVirtualSearchParams(searchParams, setSearchParams, params, navigate, location, sParams, options)
+  ];
+}
 
-  // Gives search results that are not present in the actual url, but all virtualSearchParams must be removable
-  function virtualSearchParams(key) {
-    return searchParams[key] || getVirtualObject()[key];
+// Gives search results that are not present in the actual url, but all virtualSearchParams must be removable
+function virtualSearchParams(searchParams, params, key) {
+  return searchParams[key] || getVirtualObject(searchParams, params)[key];
+}
+
+function getVirtualObject(searchParams, params, currentHeader) {
+  const header = currentHeader || params.header;
+  if (header?.match(/^(summer|fall|spring|winter)-\d+$/)) {
+    const [season, year] = header.split("-");
+    return { year, season, order: "title_romaji", sort: "ASC" }
   }
 
-  function getVirtualObject(currentHeader) {
-    const header = currentHeader || params.header;
-    if (header?.match(/^(summer|fall|spring|winter)-\d+$/)) {
-      const [season, year] = header.split("-");
-      return { year, season, order: "title_romaji", sort: "ASC" }
-    }
+  const engine = searchParams.malSearch === "true" ? "mal" : "ani";
+  return headers[header] || headers[engine]?.[header] || headers[engine]?.[params.type]?.[header] || {};
+}
 
-    const engine = searchParams.malSearch === "true" ? "mal" : "ani";
-    return headers[header] || headers[engine]?.[header] || headers[engine]?.[params.type]?.[header] || {};
+function setVirtualSearchParams(searchParams, setSearchParams, params, navigate, location, sParams, options) {
+  const virtualObj = getVirtualObject(searchParams, params);
+  const searchQueryFromObj = (...rest) => searchQueryStringFromObject(searchParams, params, location, ...rest);
+  // The user is trying to remove search results that are part of the virtual url
+  // The results are coming from params.header, so lets redirect the user to a page without the current header
+  if (keyIsNullish(sParams, ...Object.keys(virtualObj))) {
+    return navigate(`/search/${params.type}${searchQueryFromObj(sParams)}`, {scroll: false, ...options});
   }
 
+  if (params.header?.match(/^(summer|fall|spring|winter)-\d+$/)) {
+    const { season = virtualObj.season, year = virtualObj.year, ...restParams } = sParams;
+    const header = `${season}-${year}`;
+    return navigate(`/search/${params.type}/${header}${searchQueryFromObj(restParams, false, header)}`, {scroll: false, ...options});
+  }
 
-  function setVirtualSearchParams(sParams, options) {
-    const virtualObj = getVirtualObject();
-    // The user is trying to remove search results that are part of the virtual url
-    // The results are coming from params.header, so lets redirect the user to a page without the current header
-    if (keyIsNullish(sParams, ...Object.keys(virtualObj))) {
-      return navigate(`/search/${params.type}${searchQueryStringFromObject(sParams)}`, {scroll: false, ...options});
-    }
-
-    if (params.header?.match(/^(summer|fall|spring|winter)-\d+$/)) {
-      const { season = virtualObj.season, year = virtualObj.year, ...restParams } = sParams;
+  // Keep this-season in header if search has only year or season params, but if both are present switch the header to {season-year}
+  if (params.header === "this-season" || params.header === "next-season") {
+    const { season = searchParams.season, year = searchParams.year, ...restParams } = sParams;
+    if (season != null && year != null) {
       const header = `${season}-${year}`;
-      return navigate(`/search/${params.type}/${header}${searchQueryStringFromObject(restParams, false, header)}`, {scroll: false, ...options});
+      return navigate(`/search/${params.type}/${header}${searchQueryFromObj(restParams, false, header)}`, {scroll: false, ...options});
     }
-
-    // Keep this-season in header if search has only year or season params, but if both are present switch the header to {season-year}
-    if (params.header === "this-season" || params.header === "next-season") {
-      const { season = searchParams.season, year = searchParams.year, ...restParams } = sParams;
-      if (season != null && year != null) {
-        const header = `${season}-${year}`;
-        return navigate(`/search/${params.type}/${header}${searchQueryStringFromObject(restParams, false, header)}`, {scroll: false, ...options});
-      }
-    }
-
-    // No need to redirect, set search params normally
-    setSearchParams(sParams, options);
   }
 
+  // No need to redirect, set search params normally
+  setSearchParams(sParams, options);
+}
 
-  function searchQueryStringFromObject(sParams, addVirtualParams = true, header) {
-    const virtualObj = getVirtualObject(header);
-    const searchObj = Object.fromEntries(new URLSearchParams(location.search));
-    const originalSearchParams = [...new URLSearchParams(location.search)]
-    .filter(([key, value]) => {
-      if (key in sParams) {
-        return false;
-      }
-      if (!addVirtualParams && key in virtualObj && virtualObj[key] == value) {
-        return false
-      }
-      return true;
-    })
-    .map(([key, val]) => `${key}=${val}`);
+function searchQueryStringFromObject(searchParams, params, location, sParams, addVirtualParams = true, header) {
+  const virtualObj = getVirtualObject(searchParams, params, header);
+  const searchObj = Object.fromEntries(new URLSearchParams(location.search));
+  const originalSearchParams = [...new URLSearchParams(location.search)]
+  .filter(([key, value]) => {
+    if (key in sParams) {
+      return false;
+    }
+    if (!addVirtualParams && key in virtualObj && virtualObj[key] == value) {
+      return false
+    }
+    return true;
+  })
+  .map(([key, val]) => `${key}=${val}`);
 
-    const originalVirtualSearchParams = addVirtualParams === false ? [] : Object.entries(virtualObj)
+  const originalVirtualSearchParams = addVirtualParams === false ? [] : Object.entries(virtualObj)
     .filter(([key]) => {
       if (key in sParams) {
         return false;
@@ -114,25 +117,24 @@ export function useVirtualSearchParams() {
     })
     .map(([key, val]) => `${key}=${val}`);
 
-    const newSearchParams = Object.entries(sParams)
-    .filter(([, val ]) => searchValueIsNotNullish(val))
-    .map(([key, val]) => {
-      if (Array.isArray(val)) {
-        return val.map(v => `${key}=${v}`).join("&");
-      }
-      return `${key}=${val}`
-    });
-
-    const query = originalVirtualSearchParams
-    .concat(originalSearchParams)
-    .concat(newSearchParams)
-    .join("&");
-
-    if (query.length) {
-      return "?" + query;
+  const newSearchParams = Object.entries(sParams)
+  .filter(([, val ]) => searchValueIsNotNullish(val))
+  .map(([key, val]) => {
+    if (Array.isArray(val)) {
+      return val.map(v => `${key}=${v}`).join("&");
     }
-    return "";
+    return `${key}=${val}`
+  });
+
+  const query = originalVirtualSearchParams
+  .concat(originalSearchParams)
+  .concat(newSearchParams)
+  .join("&");
+
+  if (query.length) {
+    return "?" + query;
   }
+  return "";
 }
 
 function searchValueIsNotNullish(value) {
