6e43613e19fbfb51efeaf289a8d0b0376888676b
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-09-13 22:02:53 +0300

    feat: ✨ Clean up media page mode switcher and remove links from external links

--- src/components/media/ExternalLinks.jsx
+++ src/components/media/ExternalLinks.jsx
@@ -1,33 +1,14 @@
 import { ErrorBoundary, For, Match, Switch } from "solid-js";
-import { formatTitleToUrl } from "../../utils/formating";
 import "./ExternalLinks.scss";
 import Link from "../../assets/Link";
-import Anilist from "../../assets/Anilist";
-import MyAnimeList from "../../assets/MyAnimeList";
 
 export default function ExternalLinks(props) {
   return (
     <ErrorBoundary fallback="External links error">
-      <Show when={props.media}>
+      <Show when={props.hastag || props.externalLinks?.length}>
         <div>
           <h2>External links</h2>
           <ul>
-            <Show when={props.idAni}>
-              <li class="cp-external-link">
-                <div class="icon" style={{background: "#1a212b"}}>
-                  <Anilist />
-                </div>
-                <a href={`https://anilist.co/${props.type}/${props.idAni}/${formatTitleToUrl(props.title)}/`} target="_blank">AniList</a>
-              </li>
-            </Show>
-            <Show when={props.idMal}>
-              <li class="cp-external-link">
-                <div class="icon" style={{background: "#36509d"}}>
-                  <MyAnimeList />
-                </div>
-                <a href={`https://myanimelist.net/${props.type}/${props.idMal}/${formatTitleToUrl(props.title)}/`} target="_blank">MyAnimeList</a>
-              </li>
-            </Show>
             <Show when={props.hashtag}>
               <li>
                 <a href={`https://x.com/search?q=${props.hashtag}&src=typd`} target="_blank">{props.hashtag}</a>
