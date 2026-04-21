4c2efcc52cc81b4bfd9b71467c23c8f4565644b7
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-04-21 23:01:58 +0300

    dev: 👾 Added auto import + keep eslint errors
    - This might be the best middle ground. Auto imports make the code
      coping and moving really easy and then eslist errors make sure I will
      manually import all the right methods after moving something

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import solid from "vite-plugin-solid"
 import scopedStyling from "./vite-plugins/solidjs-scoped-styling";
 import autoImportScopedStyles from "./vite-plugins/auto-import-scoped-styles";
 import checker from 'vite-plugin-checker';
+import AutoImport from "unplugin-auto-import/vite"
 
 const __PORT__ = 5173;
 const __DEBUG_PORT__ = 5174;
@@ -19,36 +20,36 @@ export default defineConfig({
         lintCommand: 'eslint "./src/**/*.{js,jsx}"', // Fail on Lint errors
       },
     }),
-    // AutoImport({
-    //   include: [
-    //     /\.jsx$/,
-    //     /\.js$/,
-    //   ],
-    //   imports: [
-    //     "solid-js",
-    //     {
-    //       "@solidjs/router": [
-    //         "A", "Navigate", "useLocation", "useNavigate", "useParams"
-    //       ],
-    //     },
-    //   ],
-    //   dirsScanOptions: {
-    //     fileFilter: file => {
-    //       return file.endsWith(".js") || file.endsWith(".jsx") || file.endsWith(".ts") || file.endsWith(".tsx");;
-    //     }
-    //   },
-    //   dirs: [
-    //     "./src/assets",
-    //     "./src/components/**",
-    //     "./src/context",
-    //     "./src/hooks",
-    //     "./src/pages/**",
-    //     "./src/libs",
-    //     "./src/collections/collections.js",
-    //     "./src/utils/utils.js",
-    //     "./src/utils/api-OLD.js",
-    //   ],
-    // })
+    AutoImport({
+      include: [
+        /\.jsx$/,
+        /\.js$/,
+      ],
+      imports: [
+        "solid-js",
+        {
+          "@solidjs/router": [
+            "A", "Navigate", "useLocation", "useNavigate", "useParams"
+          ],
+        },
+      ],
+      dirsScanOptions: {
+        fileFilter: file => {
+          return file.endsWith(".js") || file.endsWith(".jsx") || file.endsWith(".ts") || file.endsWith(".tsx");;
+        }
+      },
+      dirs: [
+        "./src/assets",
+        "./src/components/**",
+        "./src/context",
+        "./src/hooks",
+        "./src/pages/**",
+        "./src/libs",
+        "./src/collections/collections.js",
+        "./src/utils/utils.js",
+        "./src/utils/api-OLD.js",
+      ],
+    })
   ],
   base: "/legendary-octo-barnacle" + (branch !== "main" ? "/branches/" + branch : ""),
   build: {
