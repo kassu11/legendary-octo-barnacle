e9035ff4b88ecc476a2215ae4421d48cb9c3581f
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-05-08 21:47:42 +0300

    refactor: 🧩 Update series airing status

--- src/pages/Home/CurrentWatchingMedia.scoped.jsx
+++ src/pages/Home/CurrentWatchingMedia.scoped.jsx
@@ -7,6 +7,7 @@ import { authedUserId, token2 } from "../../core/globalState.js";
 import { isTypeFunction } from "../../utils/functionUtils.js";
 import { createTimer, formatMSToString } from "../../utils/timeUtils.js";
 import { setFetcherValueToStorage } from "../../utils/storageUtils.js";
+import { weekInSeconds } from "../../collections/time.js";
 
 export function CurrentWatchingMediaScoped() {
   const [data, setData] = createSignal();
@@ -66,6 +67,7 @@ export function CurrentWatchingMediaScoped() {
 function parseCurrentlyWatching(res) {
   const lists = [];
   const splitCount = 5;
+  const now = new Date() / 1000;
 
   // 1. Loop manga and anime lists
   for (const row of Object.values(res.data.data)) {
@@ -75,7 +77,23 @@ function parseCurrentlyWatching(res) {
       const list = { name, entries: [] };
       entries.sort((a, b) => b.updatedAt - a.updatedAt);
       for (const entry of entries) {
-        // 3. Add entry to airing list
+        // 3. Check if entry episode has already aired
+        if (entry.media.nextAiringEpisode?.airingAt < now) {
+          // 3.1 How many episodes can still air (if not known cap to 99999)
+          const max = Math.max((entry.media.episodes || 99999) - entry.media.nextAiringEpisode.episode, 0);
+          // 3.2 How many weeks is entry behind
+          const over = Math.min(Math.ceil((now - entry.media.nextAiringEpisode.airingAt) / weekInSeconds), max);
+          // 3.3 Entry is so far back that all episodes should be out. Remove nextAiringInfo
+          if (over === max) {
+            entry.media.nextAiringEpisode = null;
+          } else {
+            // 3.4 We are behind, so add missing episode and cooldown timers (assume that series releases weekly)
+            entry.media.nextAiringEpisode.airingAt += weekInSeconds * over;
+            entry.media.nextAiringEpisode.episode += over;
+          }
+        }
+
+        // 4. Add entry to airing list
         if (entry.media.nextAiringEpisode) airing.push(entry);
         else list.entries.push(entry);
       }
@@ -84,7 +102,7 @@ function parseCurrentlyWatching(res) {
         return a.media.nextAiringEpisode.airingAt - b.media.nextAiringEpisode.airingAt;
       });
 
-      // 4. There is multiple airing anime, so lets split them
+      // 5. There is multiple airing anime, so lets split them
       if (airing.length < splitCount) list.entries.unshift(...airing);
       else lists.push({ name: name + " (Airing)", entries: airing })
 
