5a5716501584ef5a1d4238f8b42cca348d3e0bb7
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-07-12 02:00:06 +0300

    refactor: 🧩 Move dynamic graphql page generation code to its own files

--- src/collections/querys.js
+++ src/collections/querys.js
@@ -1,5 +1,7 @@
 import { objectToSearchParams } from "../utils/objectUtils";
+import { arrayUtils } from "../utils/utils";
 
+// Normal Grapql querys, no need to do anything fancy
 export { default as anilistMediaById } from "./queries/anilistMediaById.graphql";
 export { default as anilistGetActivityLikes } from "./queries/anilistGetActivityLikes.graphql";
 export { default as anilistUserSearch } from "./queries/anilistUserSearch.graphql";
@@ -48,49 +50,15 @@ export { default as getUserByName } from "./queries/getUserByName.graphql";
 export { default as profileActivity } from "./queries/profileActivity.graphql";
 export { default as currentUser } from "./queries/currentUser.graphql";
 
-export const anilistGetMediasWithIds = count => format`query ($type: MediaType, $id_in: [Int], $idMal_in: [Int]) {
-  ${[...Array(Math.ceil(count / 50))].map((_, i) => {
-    return `page${i + 1}: Page(page: ${i + 1}) {
-      media(type: $type, id_in: $id_in, idMal_in: $idMal_in) {
-        ...media
-      }
-    }`;
-  }).join(" ")}
-}
+// Basically normal grapql querys, but we want to dynamicly add pages so we can fetch over 50 entries with one query
+// For example 250 entries are split into 5 pages, but they are all inside one query
+import { default as anilistGetMediasWithIdsRaw } from "./queries/anilistGetMediasWithIds.graphql";
+export const anilistGetMediasWithIds = multiPageFetchQueryGenerator(anilistGetMediasWithIdsRaw);
 
-fragment media on Media {
-  id
-  idMal
-  type
-  bannerImage
-  title {
-    userPreferred
-  }
-  coverImage {
-    large
-  }
-}`;
+import { default as anilistGetCharacterIdsRaw } from "./queries/anilistGetCharacterIds.graphql";
+export const anilistGetCharacterIds = multiPageFetchQueryGenerator(anilistGetCharacterIdsRaw);
 
-export const anilistGetCharacterIds = count => format`query ($ids: [Int]) {
-  ${[...Array(Math.ceil(count / 50))].map((_, i) => {
-    return `page${i + 1}: Page(page: ${i + 1}) {
-      characters(id_in: $ids) {
-        ...characters
-      }
-    }`;
-  }).join(" ")}
-}
-
-fragment characters on Character {
-  id
-  name {
-    userPreferred
-  }
-  image {
-    large
-  }
-}`;
-
+// Generic Jikan and AnimeThemes post request urls
 export const animeThemesByAnilistId = ({ id }) => `https://api.animethemes.moe/anime?filter[has]=resources&filter[site]=AniList&filter[external_id]=${id}&include=animethemes.animethemeentries.videos.audio,animethemes.song.artists`;
 export const animeThemesByMyAnimeListId = ({ id }) => `https://api.animethemes.moe/anime?filter[has]=resources&filter[site]=MyAnimeList&filter[external_id]=${id}&include=animethemes.animethemeentries.videos.audio,animethemes.song.artists`;
 export const animeThemesByArtisSlug = ({ slug }) => `https://api.animethemes.moe/artist/${slug}?include=songs.animethemes.anime,songs.animethemes.animethemeentries.videos.audio,songs.animethemes.song.artists,resources,images`;
@@ -105,13 +73,12 @@ export const myAnimeListMediaSeasonSearch = ({ season, query }) => `https://api.
 export const myAnimeListMediaGenres = ({ type }) => `https://api.tenrai.org/v1/genres/${type}`;
 
 
-function format(strings, ...templates) {
-  const array = [];
-  for (let i = 0; i < strings.length; i++) {
-    array.push(strings[i], templates[i] || "");
-  }
-
-  const whitespaceAroundChar = /\s*([{}():,[\]])\s*/g;
-
-  return array.join("").replace(/\n|\r/g, " ").replace(whitespaceAroundChar, "$1").replace(/\s{2,}/g, " ");
+// The expected input contains @1, @2 and @3 texts.
+// We use these to split the text, repeat the right length of text, 
+// and place the page number in the right place
+function multiPageFetchQueryGenerator(query) {
+  return count => query.replace(/@1(.+)@3/, (_, query) => {
+    const callback = i => `page${++i}${query.replace("@2", i)}`;
+    return arrayUtils.ArrayFromRange(callback, Math.ceil(count / 50)).join("");
+  });
 }
